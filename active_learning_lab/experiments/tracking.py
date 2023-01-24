fad5149a8a7fdfca9eccf86d56b6d09edd1205f3
Maxime <maxime.arens@synapse-fr.com>
2023-01-24 09:12:41 +0100

    restoring metrics multi label

--- active_learning_lab/experiments/tracking.py
+++ active_learning_lab/experiments/tracking.py
@@ -12,10 +12,10 @@ from small_text.utils.labels import csr_to_list
 
 METRIC_COLUMNS = [
     'train_acc', 'train_micro_precision', 'train_micro_recall', 'train_micro_f1',
-    'train_macro_precision', 'train_macro_recall', 'train_macro_f1',
+    'train_macro_precision', 'train_macro_recall', 'train_macro_f1', 'train_ranking_loss', 'train_hamming_loss',
     'train_ece_10', 'test_acc', 'test_micro_precision', 'test_micro_recall', 'test_micro_f1', 'test_macro_precision',
-    'test_macro_recall', 'test_macro_f1', 'test_ece_10'
-] # , 'train_ranking_loss', 'train_hamming_loss''test_ranking_loss', 'test_hamming_loss'
+    'test_macro_recall', 'test_macro_f1','test_ranking_loss', 'test_hamming_loss', 'test_ece_10'
+] #
 
 COLUMNS = ['run_id', 'query_id', 'num_samples', 'query_time_sec', 'update_time_sec'] + \
           METRIC_COLUMNS
@@ -85,8 +85,8 @@ class MetricsTracker(object):
                 precision_score(y_true, y_pred, average='macro'),
                 recall_score(y_true, y_pred, average='macro'),
                 f1_score(y_true, y_pred, average='macro'),
-                #label_ranking_loss(y_true, csr_matrix.toarray(y_pred)),
-                #hamming_loss(y_true, y_pred),
+                label_ranking_loss(y_true, csr_matrix.toarray(y_pred)),
+                hamming_loss(y_true, y_pred),
                 expected_calibration_error(y_pred, y_pred_probas, y_true)
             ]
 
